9eb6aa720be998b5132dfaa50d1b86b5e15bb27b
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-16 20:50:48 +0600

    router set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Main from "./components/Main.jsx";
+import Phones from "./components/Phones.jsx";
+import Phone from "./components/Phone.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones></Phones>,
+      },
+      { path: "/phone", element: <Phone></Phone> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
